cf1e3eb4093710da3ae9cde0d32b0462b1bb7b08
AleKira <89973113+iAleKira@users.noreply.github.com>
2023-02-01 08:38:33 -0500

    updated script files
    updated splitInjection.sh
    updated top-services-probes.txt

--- Tesi/TextFiles/injectableProbes/splitInjection.sh
+++ Tesi/TextFiles/injectableProbes/splitInjection.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-grep "match ftp" injectable-service-probes.txt > toInject/ftp.txt
-grep "match ssh" injectable-service-probes.txt > toInject/ssh.txt
-#grep "match telnet" injectable-service-probes.txt > toInject/telnet.txt
-grep "match smtp" injectable-service-probes.txt > toInject/smtp.txt
-grep "match http" injectable-service-probes.txt > toInject/http.txt
-#grep "match dns" injectable-service-probes.txt > toInject/dns.txt
-#grep "match finger" injectable-service-probes.txt > toInject/finger.txt
-#grep "match kerberos" injectable-service-probes.txt > toInject/kerberos.txt
-#grep "match nntp" injectable-service-probes.txt > toInject/nntp.txt
-#grep "match netbios" injectable-service-probes.txt > toInject/netbios.txt
-#grep "match imap" injectable-service-probes.txt > toInject/imap.txt
-#grep "match snmp" injectable-service-probes.txt > toInject/snmp.txt
-#grep "match ldap" injectable-service-probes.txt > toInject/ldap.txt
-#grep "match directconnect" injectable-service-probes.txt > toInject/directconnect.txt
-#grep "match microsoft-d" injectable-service-probes.txt > toInject/microsoft-d.txt
-#grep "match rtsp" injectable-service-probes.txt > toInject/rtsp.txt
-#grep "match nntp" injectable-service-probes.txt > toInject/nntp.txt
-#grep "match ipp" injectable-service-probes.txt > toInject/ipp.txt
-#grep "match pop3" injectable-service-probes.txt > toInject/pop3.txt
+rm toInject/*
+for i in "ftp" "ssh" "telnet" "smtp" "http" "dns" "kerberos" "nntp" "netbios" "imap" "snmp" "ldap" "directconnect" "microsoft-d" "rtsp" "nntp" "ipp" "pop3"
+do
+  grep "match $i" injectable-service-probes.txt > toInject/"$i".txt
+  filesize=$(stat -c %s $"toInject/"$i".txt")
+  if [[ $filesize == "0" ]] ; then 
+  rm "toInject/"$i".txt"
+  else 
+  lines=$(grep "" -c $"toInject/"$i".txt")
+  echo "Service $i has $lines injectable directives."
+  fi
+done
